d6db47a24006f786ff841889a7335cb6283b6469
SauloSTS <sts_10@hotmail.com>
2021-08-09 20:16:35 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraTeste/Controllers/CalculadoraController.cs
@@ -0,0 +1,42 @@
+﻿using CalculadoraTeste.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculadoraTeste.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculadoraViewModel calculadoraViewModel)
+        {
+            ModelState.Clear();
+            if (calculadoraViewModel.Operador.Equals("+"))
+            {
+                calculadoraViewModel.Resultado = calculadoraViewModel.Num1 + calculadoraViewModel.Num2;
+            }
+            if (calculadoraViewModel.Operador.Equals("-"))
+            {
+                calculadoraViewModel.Resultado = calculadoraViewModel.Num1 - calculadoraViewModel.Num2;
+            }
+            if (calculadoraViewModel.Operador.Equals("*"))
+            {
+                calculadoraViewModel.Resultado = calculadoraViewModel.Num1 * calculadoraViewModel.Num2;
+            }
+            if (calculadoraViewModel.Operador.Equals("/"))
+            {
+                calculadoraViewModel.Resultado = calculadoraViewModel.Num1 / calculadoraViewModel.Num2;
+            }
+            return View(calculadoraViewModel);
+        }
+
+    }
+}
---
+++ CalculadoraTeste/Models/CalculadoraViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalculadoraTeste.Models
+{
+    public class CalculadoraViewModel
+    {
+        /// <summary>
+        /// Primeiro número do calculo
+        /// </summary>
+        [Display(Name ="Numero 1")]
+        public int Num1 { get; set; }
+        /// <summary>
+        /// segundo número do calculo
+        /// </summary>
+        [Display(Name = "Numero 2")]
+        public int Num2 { get; set; }
+        /// <summary>
+        /// Tipo de operador( + - * / ) que vai ser realizado
+        /// </summary>
+        public string Operador { get; set; }
+        /// <summary>
+        /// Resultado da operação
+        /// </summary>
+        public int Resultado { get; set; }
+    }
+}
